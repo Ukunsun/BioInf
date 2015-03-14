5686f4c0f9a2a1acb6e33b0cfc8cea8258e929d4
Ukunsun <Ukunun@gmail.com>
2015-03-14 12:58:09 +0300

    Карп-Рабин

--- labs3/lol.hs
+++ labs3/lol.hs
@@ -18,22 +18,33 @@ nativeSearch (el:str) pat i res = let
 					then nativeSearch (drop (len-1) str) pat (i+len)(res++[i])
 					else nativeSearch str pat (i+1) res
 
+ {- 
+ 1 function RabinKarp(string s[1..n], string sub[1..m])
+ 2     hsub := hash(sub[1..m])
+ 3     hs := hash(s[1..m])
+ 4     for i from 1 to (n-m+1)
+ 5         if hs = hsub
+ 6             if s[i..i+m-1] = sub
+ 7                 return i
+ 8         hs := hash(s[i+1..i+m])
+ 9     return not found
+ -}
 kRA::String->String->[Int]
 kRA str pat = let
 			m = length pat;
-			hSub = hash pat m;
+			hSub = hash pat m; -- 2
 			
-			f::String->Int->Int->Int->[Int]->[Int]
-			f [] _ _ _ res = res
-			f str2 hS hs0 i res = let
-					hs = hash (take m str2) m;
-				in if(hS == hs)
-				then if((take m $drop i str)==pat)
-					then f (drop m str2) hS hs (i+m) (res++[i])
-					else f (drop 1 str2) hS hs (i+1) res
-				else f (drop 1 str2) hS hs (i+1) res
-			
-			in f str hSub 0 0 []
+			f::String->Int->Int->[Int]->[Int]
+			f [] _ _ res = res
+			f str2 hS i res = let
+					hs = hash (take m str2) m; -- 3
+				in if(hS == hs) --- 5
+				then if((take m $drop i str)==pat) -- 6
+					then f (drop m str2) hS (i+m) (res++[i])
+					else f (drop 1 str2) hS (i+1) res
+				else f (drop 1 str2) hS(i+1) res
+
+			in f str hSub 0 []
 
 {- fuckingKarpRabinAlg::String->Int-> Int->Int->[Int]->[Int]
 fuckingKarpRabinAlg [] len hSub  hs res = res
@@ -45,6 +56,19 @@ fuckingKarpRabinAlg str len hSub  hs res | []
 				then 
 				else fuckingKarpRabinAlg str len hSub  hs res -}
 
+{-
+> ord 'a' 
+97
+> ord 'b'
+98
+> ord 'c'
+99
 
+(zip str [0,1..length str])
+фигачим пары (Char,Int) (символ, порядковый номер)
+
+-}
 hash::String->Int->Int
 hash str m = sum $ map(\(x,y)-> (ord x)*m^y) (zip str [0,1..length str])
+
+
