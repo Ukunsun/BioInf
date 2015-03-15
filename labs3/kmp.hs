c15c1795dc1acd654a9c43a6aa56a1c320faa33c
Ukunsun <Ukunun@gmail.com>
2015-03-15 15:42:15 +0300

    Кнут-Моррис-Пратт
    Открыть википедию Ctrl+C, Ctrl+V
    хотя, кажется, разобрался с ByteString и прочими плюшками)

---
+++ labs3/kmp.hs
@@ -0,0 +1,42 @@
+-- https://ru.wikibooks.org/wiki/%D0%A0%D0%B5%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8_%D0%B0%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC%D0%BE%D0%B2/%D0%90%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC_%D0%9A%D0%BD%D1%83%D1%82%D0%B0_%E2%80%94_%D0%9C%D0%BE%D1%80%D1%80%D0%B8%D1%81%D0%B0_%E2%80%94_%D0%9F%D1%80%D0%B0%D1%82%D1%82%D0%B0
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
+import qualified Data.ByteString.Char8 as BS
+ 
+type PrefixTable = V.Vector Int
+ 
+makePrefixTable :: BS.ByteString -> PrefixTable
+makePrefixTable s =
+  let n = BS.length s
+  in V.create $ do
+    p <- VM.new n
+    VM.set p 0
+    let loop i k | i == n = return ()
+                 | k > 0 && (BS.index s i /= BS.index s k) = do
+                     nk <- VM.read p (k - 1)
+                     loop i nk
+                 | otherwise = do
+                     let nk = if BS.index s i == BS.index s k
+                              then k + 1
+                              else k
+                     VM.write p i nk
+                     loop (i + 1) nk
+ 
+    loop 1 0
+    return p
+ 
+indexOf :: BS.ByteString -> BS.ByteString -> Int
+indexOf needle haystack =
+  let pt    = makePrefixTable needle
+      hLen  = BS.length haystack
+      nLen  = BS.length needle
+      loop i k | k == nLen = i - nLen
+               | i == hLen = -1
+               | k > 0 && (BS.index needle k /= BS.index haystack i) =
+                   loop i $ pt V.! (k - 1)
+               | otherwise =
+                   let nk = if BS.index needle k == BS.index haystack i
+                            then k + 1
+                            else k
+                   in loop (i + 1) nk
+  in loop 0 0
--- labs3/lol.hs
+++ labs3/lol.hs
@@ -34,10 +34,10 @@ kRA str pat = let
 			m = length pat;
 			hSub = hash pat m; -- 2
 			
-			f::String->Int->Int->[Int]->[Int]
+			f::String->Int->Int->[Int]->[Int] -- 4
 			f [] _ _ res = res
 			f str2 hS i res = let
-					hs = hash (take m str2) m; -- 3
+					hs = hash (take m str2) m; -- 3,8
 				in if(hS == hs) --- 5
 				then if((take m $drop i str)==pat) -- 6
 					then f (drop m str2) hS (i+m) (res++[i])
@@ -45,16 +45,6 @@ kRA str pat = let
 				else f (drop 1 str2) hS(i+1) res
 
 			in f str hSub 0 []
-
-{- fuckingKarpRabinAlg::String->Int-> Int->Int->[Int]->[Int]
-fuckingKarpRabinAlg [] len hSub  hs res = res
-fuckingKarpRabinAlg str len hSub  hs res | []
-				| (length str)<=len = res
-				| otherwise = let
-				hs
-				in if(hs==hSub)
-				then 
-				else fuckingKarpRabinAlg str len hSub  hs res -}
 
 {-
 > ord 'a' 
@@ -65,10 +55,47 @@ fuckingKarpRabinAlg str len hSub  hs res | []
 99
 
 (zip str [0,1..length str])
-фигачим пары (Char,Int) (символ, порядковый номер)
+пары (Char,Int) (символ, порядковый номер)
 
 -}
 hash::String->Int->Int
 hash str m = sum $ map(\(x,y)-> (ord x)*m^y) (zip str [0,1..length str])
 
 
+
+{- bm::String->String->Int
+bm str pat = let
+			tbl = []
+		in 0
+checkHigh::String->String->Int->Int
+checkHigh str pat m = if(str!!m == (last pat))
+		then 0
+		else checkInside str pat m
+
+checkInside::String->String->Int->Int
+checkInside str pat m = let
+			el = str!!m;
+			-- num = элементу из таблицы смещений
+			-- num = get tbl el;
+			num = last $findIndices (== el) str;
+			in if (elem el pat) 
+			then checkHigh str pat (m+num) -- checkFromNum str pat m num;
+			else checkHigh str pat (m + length pat)
+
+-- checkFromNum::String->String->Int->Int->
+-- checkFromNum str pat m num
+
+get::[(Char,Int)]->Char->Int
+get [] el = -1
+get ((x,y):lst) el = if (x == el)
+			then y
+			else get lst el
+			
+checkEl::String->String->Int->Int
+checkEl str pat m =  let
+		m = length pat;
+		elM = str!!m;
+		elP = str!!m;
+		in if(elM==elP)
+		then 0
+		else 0 -}
